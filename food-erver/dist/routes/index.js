445d5574c9a58e67d1c846868fbb7a33dddeb50c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-10-05 13:05:53 +1000

    setup

---
+++ food-erver/dist/routes/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = _express["default"].Router();
+/* GET home page. */
+
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+var _default = router;
+exports["default"] = _default;
